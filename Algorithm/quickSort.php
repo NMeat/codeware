1e7c249dcd88ab8d99888b4229d7b4ea16e8c18b
laucie <517834118@qq.com>
2016-03-16 22:57:57 +0800

    快速排序

---
+++ Algorithm/quickSort.php
@@ -0,0 +1,27 @@
+<?php
+//快速排序
+function quickSort($arr){
+	$len = count($arr);
+	if(count($arr) <= 1) return $arr;
+	$first = $arr[0];
+	$left = $right = array();
+	for($i = 1; $i < $len; $i++){
+		if ($arr[$i] < $first) {
+			$left[] = $arr[$i];
+		}else{
+			$right[] = $arr[$i];
+		}
+	}
+
+	$left  = quickSort($left);
+	$right = quickSort($right);
+	$res = array_merge($left, array($first), $right);
+	return $res;
+}
+
+$arr = array(23,4,3,2,78,4556,34,22,987);
+
+$arr = quickSort($arr);
+
+var_dump($arr);
+
