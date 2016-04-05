80e97a248eb0de1b4fd2cd9ce2e03e94b722e95c
laucie <517834118@qq.com>
2016-04-05 14:08:21 +0800

    小修改

--- Linux/ssh_login.sh
+++ Linux/ssh_login.sh
@@ -6,13 +6,31 @@ set timeout xxx
 set port xxx  
 spawn ssh $user@$ip -p $port   
 expect {               
- 	"*password:" { send "$password\r" }      //出现密码提示,发送密码  
+ 	"*password:" { send "$password\r" }      
 	"*yes/no" { 
+		send "yes\r)?"; exp_continue
+		set timeout 1
+		expect{
+			"*password" { send "$password\r"}
+		}
+	}  										
+}  
+interact          							
+
+set user xxx
+set ip xxx.xxx.xxx.xxx  
+set password xxx 
+set timeout xxx
+set port xxx  
+spawn ssh $user@$ip -p $port   
+expect {               
+ 	"*password:" { send "$password\r" }      
+	"*yes/no)?" { 
 		send "yes\r"; exp_continue
 		set timeout 1
 		expect{
 			"*password" { send "$password\r"}
 		}
-	}  //第一次ssh连接会提示yes/no,继续  
+	}  										
 }  
-interact          //交互模式,用户会停留在远程服务器上面.  
+interact
