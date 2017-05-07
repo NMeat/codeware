f9c49731f03766c98e81e0be8f0c677895e820bb
laucie <517834118@qq.com>
2017-05-07 23:02:55 +0800

    PHP多进程

--- PHP/multi_process.php
+++ PHP/multi_process.php
@@ -1,62 +1,48 @@
 <?php
 //PHP的多进程
-class StatInfo{
+class StatInfo
+{
     public static $procContain = 20000;
-    public static $maxProc = 10;
-    public static $isChild = null;
-    protected static $run = true;
+    public static $maxProc = 5;
     protected static $subProcessed = array();
 
     public function run(){
-        $begin_process = 0;//进程开始个数
-        self::$subProcessed = array();
-        $procCount = 0;
-        while ($procCount < self::$maxProc) {
-            $pid = pcntl_fork();
-            $begin_process++;
+        $begin_process = $procCount = 1;
+
+        //创建5个进程
+        while ($procCount <= self::$maxProc) {
+            $pid = pcntl_fork();   //创建进程
+            ++$begin_process;
             ++$procCount;
-            if(0 == $pid){//child
+
+            if(0 == $pid){  //子进程会返回0
                 //这里是子进程执行的逻辑
-                self::$isChild = true;
-                $this->forkRun(self::$procContain*($procCount-1));
-            }else if(0 < $pid){//parent
+                $this->forkRun(self::$procContain * ($procCount - 1));
+                exit(0);    //子进程退出
+            }else if(0 < $pid){     //父进程会得到大于0的子进程id号
                 //这里是父进程执行的逻辑
-                self::$isChild = false;
                 self::$subProcessed[] = $pid;
-                continue;
-            }else{
-                break;
+            }else{  //创建子进程失败会返回-1
+                echo '创建子进程失败' . "\n";
             }
         }
 		
-        //管理子进程状态,$begin_process记录了启动了几个子进程
-        //当返回一个 $begin_process--,当$begin_process=0时子进程全部结束了。
-        while (self::$run) {
-            $status = 0;
-            $pid = pcntl_waitpid(-1, $status, WUNTRACED);
-            if (0 <= $pid) {
-                $begin_process--;
-            }
-            if ($begin_process == 0){
-                self::$run = false;
+        while (true) {
+            $res = pcntl_waitpid(-1, $status, WUNTRACED);
+            if($res == 0 || $res == -1){
                 break;
             }
         }
     }
 
-	 public function forkRun($start) {
-        $offset = 1;
-        $count = 200;
-
-        while ($offset < 5) {
-            $begin = $start+$offset;
-            $end = $begin+self::$procContain;
-            echo $end . "\n";
-			++$offset;
+	 public function forkRun($start){
+        $start_set = 1;
+        while ($start_set <= 5) {
+            echo $start . "\n";
 			sleep(rand(1,5));
+            ++$start_set;
 		}
-		exit;
 	}
 }
-$obj = new StatInfo;
+$obj = new StatInfo();
 $obj->run();
