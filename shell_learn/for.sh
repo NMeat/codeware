60298efcaf74be83266d4c492d69557030453068
laucie <517834118@qq.com>
2017-05-17 19:25:25 +0800

    shell学习

--- shell_learn/for.sh
+++ shell_learn/for.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 echo 'please input a number:'
 read number
-for((i=0; i< $number; i++));do
-echo $i
+for((i=0; i< $number; i++));
+do
+	echo $i;
+done
+
+for skill in php,js,python;
+do
+    echo "i am good at ${skill} script"
 done
--- shell_learn/test1.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-for skill in Sdd,ddd,ee,rr;do
-	echo "i an good at ${skill} script"
-done
