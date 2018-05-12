1edcea2029eee94cab44a3a7e07ac4cea93535f6
laucie <517834118@qq.com>
2018-05-12 19:48:17 +0800

    工厂模式

--- PHP/设计模式/factory.php
+++ PHP/设计模式/factory.php
@@ -1,39 +1,65 @@
 <?php
 /*
-    工厂模式:
-        提供获取某个对象实例的一个接口，同时使调用代码避免确定实例化基类的步骤。
-        工厂模式 实际上就是建立一个统一的类实例化的函数接口。统一调用，统一控制。
-        工厂模式是php项目开发中，最常用的设计模式，一般会配合单例模式一起使用，来加载php类库中的类
-    使用场景：
-        我们拥有一个Json类，String类，Xml类。
-        如果我们不使用工厂方式实例化这些类，则需要每一个类都需要new一遍，过程不可控，类多了，到处都是new的身影
-        引进工厂模式，通过工厂统一创建对象实例
-*/
+与单例模式一样，工厂模式也是一种创建对象的模式。它的特点在于把创建对象的过程封装了起来，从而减少了不同类之间的耦合。
 
-//工厂模式 提供获取某个对象实例的一个接口，同时使调用代码避免确定实例化基类的步骤  
-//字符串类  
-class String {  
-    public function write() {
-        return 'String'
-    }  
-}  
-//Json类  
-class Json {  
-    public function getJsonData() {
-        return 'Json';
-    }  
-}  
-//xml类  
-class Xml {  
-    public function buildXml() {
-        return 'Xml';
-    }  
-}  
-//工厂类  
-class Factory {  
-    public static function create($class) {  
-        return new $class;  
-    }  
-}  
-$ob = Factory::create("Json"); //获取Json对象
-$ob->getJsonData();   
+例如，不使用工厂模式，很多地方调用类 A，那么就要实例化一个对象：new A($construct)。如果某天需要修改 A 的名称或者参数，意味着这些代码都要修改。而使用了工厂模式，我们的对象都是从工厂类中产生的，这时我们只要修改工厂类的代码就行。
+
+工厂模式常见的应用场景是这样的：有一个基类（抽象商品）以及多个继承基类的子类（具体商品），例如多个数据库，多种支付方式等。
+*/ 
+
+
+//商品基类
+abstract class Cat
+{
+    abstract protected function voice();
+}
+
+//子类  白猫
+class WhiteCat extends Cat
+{
+    public function voice()
+    {
+        echo '我是白猫, 喵喵喵' . PHP_EOL;
+    }
+}
+
+//子类 黑猫
+class BlackCat extends Cat
+{
+    public function voice()
+    {
+        echo '我是黑猫，喵喵喵' . PHP_EOL;
+    }
+}
+
+//工厂类 生产对象
+class factory
+{
+    public static function create($colorCat)
+    {
+        switch ($colorCat) {
+            case 'WhiteCat':
+                return new WhiteCat();
+                break;
+            case 'BlackCat':
+                return new BlackCat();
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
+
+    public static function createWhiteCat(){
+        return new WhiteCat();
+    }
+
+    public static function createBlackCat(){
+        return new BlackCat();
+    }
+}
+
+$WhiteCat = factory::create('WhiteCat');
+$WhiteCat->voice();
+$BlackCat = factory::create('BlackCat');
+$BlackCat->voice();
