78a5a096c611820597ae7db6cd63626deeee101c
uzwind <lautuman@gmail.com>
2019-11-29 11:28:49 +0800

    node.js demo

--- node_demo/hello.module.js
+++ node_demo/hello.module.js
@@ -3,10 +3,19 @@
 exports 只能使用.号对外暴露内部变量
 module.exports 既可以用.号也可以用 module.exports = {}的方式
 */
-exports.hello = "hello world"
+exports.msg = "hello world"
 
-exports.add = function (a, b) {
+module.exports.add = function (a, b) {
     return a+b;
 }
 
-module.exports.name = "china boy"
+function hello(){
+    console.log('hello word');
+}
+
+function greet(name){
+    console.log('hello-------'+name);
+}
+
+exports.hello = hello;
+exports.greet = greet;
