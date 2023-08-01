016314d7893af57c474706e2462be57add0d6b14
uzwind <lautuman@gmail.com>
2023-08-01 22:58:47 +0800

    go demo

--- Go/Test/test2_test.go
+++ Go/Test/test2_test.go
@@ -3,6 +3,7 @@ package demo
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 )
 
 type Animal interface {
@@ -21,15 +22,15 @@ func (d Dog) Move() string {
 }
 
 func Test02(t *testing.T) {
-	//dog := new(Dog)
-	//fmt.Println(dog.Move())
-	//fmt.Printf("%T\n", dog)
-	//
-	//var dog2 Dog
-	//fmt.Println(dog2.Move())
-	//fmt.Printf("%T\n", dog2)
-	//
-	//var e struct{}
-	//fmt.Println(e) // 匿名结构体
-	//fmt.Println(unsafe.Sizeof(e))
+	dog := new(Dog)
+	fmt.Println(dog.Move())
+	fmt.Printf("%T\n", dog)
+
+	var dog2 Dog
+	fmt.Println(dog2.Move())
+	fmt.Printf("%T\n", dog2)
+
+	var e struct{}
+	fmt.Println(e) // 匿名结构体
+	fmt.Println(unsafe.Sizeof(e))
 }
